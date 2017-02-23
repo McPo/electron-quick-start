50982e3b5d0f1c311ad54fadd822772aa32d5fec
Emmet McPoland <emcpoland@gmail.com>
2017-02-23 11:40:00 +0000

    Added a test script for sharp issue #719

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const sharp = require("sharp");
+
+sharp('test.webp')
+    .toBuffer()
+    .catch(err => console.error(err))
+    .then(data => console.log(data.toString('base64')));
